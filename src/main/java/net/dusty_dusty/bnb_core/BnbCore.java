8a1d7014c593b54d3fd86c5029cc7fb1763644a9
Ada Aster <an.ada.poirier@gmail.com>
2026-04-20 18:27:05 -0400

    Refresh DH Color Cache on season change

--- src/main/java/net/dusty_dusty/bnb_core/BnbCore.java
+++ src/main/java/net/dusty_dusty/bnb_core/BnbCore.java
@@ -1,8 +1,10 @@
 package net.dusty_dusty.bnb_core;
 
 import com.mojang.logging.LogUtils;
+import com.seibel.distanthorizons.api.DhApi;
 import com.seibel.distanthorizons.api.methods.events.DhApiEventRegister;
 import com.seibel.distanthorizons.api.methods.events.abstractEvents.DhApiChunkProcessingEvent;
+import glitchcore.event.EventManager;
 import net.dusty_dusty.bnb_core.cold_crops.ColdCrops;
 import net.dusty_dusty.bnb_core.cold_crops.data.CropsNSeedsData;
 import net.dusty_dusty.bnb_core.cold_crops.network.PacketChannel;
@@ -16,6 +18,7 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 import org.slf4j.Logger;
 
 import net.minecraftforge.eventbus.api.SubscribeEvent;
+import sereneseasons.api.season.SeasonChangedEvent;
 
 // The value here should match an entry in the META-INF/mods.toml file
 @Mod(BnbCore.MODID)
@@ -27,10 +30,11 @@ public class BnbCore
     public BnbCore(FMLJavaModLoadingContext context )
     {
         IEventBus modEventBus = context.getModEventBus();
-        modEventBus.addListener(this::commonSetup);
+        modEventBus.addListener( this::commonSetup );
 
         IEventBus ForgeEventBus = MinecraftForge.EVENT_BUS;
-        ForgeEventBus.register(this);
+        ForgeEventBus.register( this );
+        EventManager.addListener( this::onSeasonChange );
 
         DhApiEventRegister.on( DhApiChunkProcessingEvent.class, new DhBlockFixer() );
 
@@ -44,5 +48,9 @@ public class BnbCore
     @SubscribeEvent
     public void jsonReading(AddReloadListenerEvent event) {
         event.addListener(CropsNSeedsData.instance);
+    }
+
+    public void onSeasonChange( SeasonChangedEvent event ) {
+        DhApi.Delayed.renderProxy.clearRenderDataCache();
     }
 }
